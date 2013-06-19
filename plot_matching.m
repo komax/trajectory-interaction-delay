c75f08068cbc769e29ea28c6fe95a8223300e63c
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-06-19 15:38:12 +1000

    minor typo

--- plot_matching.m
+++ plot_matching.m
@@ -1,8 +1,8 @@
 function plot_matching(t1,t2, matching)
 figure
 hold on;
-scatter3(t1(:,1),t1(:,2),t1(:,3),5,t1(:,4);
-scatter3(t2(:,1),t2(:,2),t2(:,3),5,t2(:,4);
+scatter3(t1(:,1),t1(:,2),t1(:,3),5,t1(:,4));
+scatter3(t2(:,1),t2(:,2),t2(:,3),5,t2(:,4));
 
 for n = 1:length(matching)
     i = matching(1,n);
