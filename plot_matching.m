a87f6e7bb2dbd62c3f92f2b45f2e739c4eb3d3dc
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-06-20 15:06:29 +1000

    fixed bug with missing 'j'

--- plot_matching.m
+++ plot_matching.m
@@ -7,7 +7,7 @@ plot3(t2(:,1),t2(:,2),t2(:,3),'k');
 for n = 1:length(matching)
     i = matching(n,1) + 1;
     j = matching(n,2) + 1;
-    plot3([t1(i,1); t2(i,1)],[t1(i,2); t2(i,2)],[t1(i,3); t2(i,3)],'-')
+    plot3([t1(i,1); t2(j,1)],[t1(i,2); t2(j,2)],[t1(i,3); t2(j,3)],'-')
 end
 
 scatter3(t1(:,1),t1(:,2),t1(:,3),30,t1(:,4),'filled');
