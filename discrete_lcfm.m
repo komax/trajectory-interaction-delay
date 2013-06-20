c1e98180178a6f46043338480ffe69ac0eaf8c93
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-06-20 15:10:51 +1000

    seems like locally correct matching works =)

--- discrete_lcfm.m
+++ discrete_lcfm.m
@@ -8,5 +8,17 @@ function matching = compute_discrete_locally_correct_frechet(t1, t2)
 javaaddpath .
 import frechet.*;
 
-grid = compute_distance_terrain(t1, t2);
-matching = LocallyCorrectFrechet.compute(grid, t1, t2);
+%%%grid = compute_distance_terrain(t1, t2);
+grid = zeros(length(t1),length(t2));
+for i = 1:length(t1)
+    for j = 1:length(t2)
+        p = t1(i,1:3);
+        q = t2(j,1:3);
+        d = norm(p - q,2);
+        grid(i,j) = d;
+    end
+end
+rows = length(t1);
+cols = length(t2);
+
+matching = LocallyCorrectFrechet.compute(grid, t1, t2, rows, cols);
--- frechet/LocallyCorrectFrechet.java
+++ frechet/LocallyCorrectFrechet.java
@@ -5,27 +5,34 @@ import frechet.*;
 
 public class LocallyCorrectFrechet {
 
-    public static Matching compute(double[][] grid, int[][] t1, int[][] t2) {
-        int n = grid[0].length;
-        int m = grid.length;
-        PathTree tree = new PathTree(grid, n, m);
+    public static Matching compute(double[][] grid, int[][] t1, int[][] t2, int numRows, int numColumns) {
+        if ((grid[0].length != numColumns) || (grid.length != numRows)) {
+            System.err.printf("Size of grid and size of trajectories disagree\n");
+            return null;
+        }
+        if ((t1.length != numColumns) || (t2.length != numRows)) {
+            System.err.printf("Size of trajectories something blah huh?\n");
+        }
+///        System.out.printf("Rows: %d columns: %d\n",numRows,numColumns);
+        PathTree tree = new PathTree(grid, numRows, numColumns);
 
         
-        for (int i = 1; i < m; i++ ) {
+        for (int i = 1; i < numRows ; i++ ) {
             tree.add(i,0);
         }
-        for (int j = 1; j < n; j++) {
+        for (int j = 1; j < numColumns ; j++) {
             tree.add(0,j);
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < m; j++) {
+        for (int i = 1; i < numRows ; i++) {
+            for (int j = 1; j < numColumns ; j++) {
+                
                 tree.add(i,j);
             }
         }
 
 
         ArrayList<Node> path = new ArrayList<Node>();
-        Node node = tree.grid[n-1][m-1];
+        Node node = tree.grid[numRows-1][numColumns-1];
         while (node != null) {
             path.add(node);
             node = node.parent;
