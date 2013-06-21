6ed168a96ad7ddc3a887f72e593a4aa185b7ff07
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-06-22 09:06:24 +1000

    fixed plot

--- plot_matching.m
+++ plot_matching.m
@@ -1,13 +1,19 @@
-function plot_matching(t1,t2, matching)
+function [distances, delay] = plot_matching(t1,t2, matching)
 figure
 hold on;
 plot3(t1(:,1),t1(:,2),t1(:,3),'k');
 plot3(t2(:,1),t2(:,2),t2(:,3),'k');
-
+distances = [];
+delay = [];
 for n = 1:length(matching)
     i = matching(n,1) + 1;
     j = matching(n,2) + 1;
     plot3([t1(i,1); t2(j,1)],[t1(i,2); t2(j,2)],[t1(i,3); t2(j,3)],'-')
+    p1 = t1(i,1:3);
+    p2 = t2(j,1:3);
+    d = norm(p1 - p2, 2);
+    delay(n) = t1(i,4) - t2(j,4);
+    distances(n) = d;
 end
 
 scatter3(t1(:,1),t1(:,2),t1(:,3),30,t1(:,4),'filled');
