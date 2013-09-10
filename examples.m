59ef116e179ba72aabb164280119f8ec428da949
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-09-10 18:16:16 +1000

    added code to visualise path through distance terrain

--- examples.m
+++ examples.m
@@ -15,4 +15,28 @@ title('Comparison of original and smoothed trajectory (bird 5)');
 plot_trajectory(trajectory);
 plot_trajectory(smoothed_trajectory,'r');
 
+% given two trajectories, compute the distance terrain
+bird_i = smoothing.apply_butterworth(get_bird(11), 2, 0.1);
+bird_j = smoothing.apply_butterworth(get_bird(15), 2, 0.1);
+figure; hold on;
+plot_trajectory(bird_i,'b');
+plot_trajectory(bird_j,'r');
+
+terrain = matching.compute_distance_terrain(bird_i, bird_j);
+figure; imagesc(terrain);
+
+% given the distance terrain estimate the Frechet distance 
+% (using binary search and DfS)
+d_ij = matching.estimate_frechet_distance(terrain);
+disp(['Estimated Frechet distance for birds 11 and 15: ', num2str(d_ij)]);
+
+% compute a locally correct Frechet matching
+matching_ij = matching.discrete_lcfm(bird_i, bird_j);
+
+
+% visualise a locally correct Frechet matching
+figure;
+visualise.plot_matching(bird_i, bird_j, matching_ij);
+figure;
+visualise.plot_path(bird_i, bird_j, matching_ij);
 
