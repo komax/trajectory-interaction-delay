baeec75edc27fcdc8f1662c94995eda1b6228dff
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-10-03 09:03:36 +1000

    changed example_smoothing to also accept optional cutoff parameter

--- example_smoothing.m
+++ example_smoothing.m
@@ -1,12 +1,17 @@
-function example_smoothing(bird_id)
+function example_smoothing(bird_id, varargin)
 %%%function example_smoothing(bird_id)
 % given a trajectory, apply a Butterworth filter to smooth out changes in position
 % 5 is the trajectory with bird_id 5 in the first data set. 
 % 21 (e.g. 5 + 16) is the trajectory with bird_id 5 in the second set.
     trajectory = get_bird(bird_id); 
 
+
     order = 2;
-    normalised_cutoff = 0.1; % should be between 0 and 1
+    if nargin > 1
+        normalised_cutoff = varargin{1}
+    else
+        normalised_cutoff = 0.1; % should be between 0 and 1
+    end
     smoothed_trajectory = smoothing.apply_butterworth(trajectory, order, normalised_cutoff);
 
     % compare smoothed (red) and original (blue) trajectories
