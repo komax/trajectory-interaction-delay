3c462e6b7773104c63d2443a2d6e29fa7ad4c641
Georgina Wilcox <g.wilcox@usyd.edu.au>
2013-10-09 11:43:32 +1100

    only interested in overlapping points for plotting simple frechet

--- +visualise/plot_frechet.m
+++ +visualise/plot_frechet.m
@@ -4,8 +4,9 @@ function results =  stuff(birds, num_birds)
         bird_i = birds(birds(:,5) == i,:);
         for j = i+1:num_birds
             bird_j = birds(birds(:,5) == j,:);
+            [t1, t2] = matching.compute_overlap(bird_i, bird_j);
             fprintf(1,'Estimating Frechet distance for birds %d and %d\n',i,j);
-            terrain = matching.compute_distance_terrain(bird_i, bird_j);
+            terrain = matching.compute_distance_terrain(t1, t2);
             results(i,j) = matching.estimate_frechet_distance(terrain);
 
             save frechet_distances.mat results;
