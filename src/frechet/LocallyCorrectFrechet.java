2c208ce99fe4a1dd34234615597639f9d4859e75
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-16 13:39:40 +0200

    Throwing an error if size of matrices do not match

--- src/frechet/LocallyCorrectFrechet.java
+++ src/frechet/LocallyCorrectFrechet.java
@@ -1,13 +1,13 @@
 package frechet;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class LocallyCorrectFrechet {
 
     public static Matching compute(double[][] grid, double[][] t1, double[][] t2, int numRows, int numColumns) {
         if ((grid[0].length != numColumns) || (grid.length != numRows)) {
-            System.err.printf("Size of grid and size of trajectories disagree\n");
-            return null;
+            throw new RuntimeException("Size of grid and size of trajectories disagree\n");
         }
         PathTree tree = new PathTree(grid, numRows, numColumns);
 
@@ -19,14 +19,13 @@ public class LocallyCorrectFrechet {
             tree.add(0,j);
         }
         for (int i = 1; i < numRows ; i++) {
-            //System.out.printf("Adding (%d, j)\n", i);
             for (int j = 1; j < numColumns ; j++) {
                 tree.add(i,j);
             }
         }
 
 
-        ArrayList<Node> path = new ArrayList<Node>();
+        List<Node> path = new ArrayList<Node>();
         Node node = tree.grid[numRows-1][numColumns-1];
         while (node != null) {
             path.add(node);
