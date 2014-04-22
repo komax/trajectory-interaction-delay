4bedea83517985b5b7cf140069aacc7995a508f8
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-22 13:38:10 +0200

    Computing a LCFM on the two bat trajectories

--- plotMatchingBats.m
+++ plotMatchingBats.m
@@ -1,5 +1,11 @@
-
+% Load data for the bats from file.
 [trajA, trajB, timeIndices] = loadBatData();
 
+% Pad the 2D trajectories to 3D ones.
+trajA = pad2DTrajectoryTo3D(trajA);
+trajB = pad2DTrajectoryTo3D(trajB);
+
+% Compute a Locally Correct Frechet Matchting.
 lcfMatching = matching.discrete_lcfm(trajA,trajB);
-visualise.plot_matchin(trajA,trajB,lcfMatching);
+% Plot the trjactories with its lcf matching.
+visualise.plot_matching(trajA,trajB,lcfMatching);
