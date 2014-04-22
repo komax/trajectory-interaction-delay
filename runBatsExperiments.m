8fa463dfbb255ec3cb87ed80d7ffd4dbad029d51
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-22 15:43:37 +0200

    Load data if flag does not exist and add of plotting of distance terrain

--- runBatsExperiments.m
+++ runBatsExperiments.m
@@ -1,11 +1,19 @@
 % Load data for the bats from file.
-[trajA, trajB, timeIndices] = loadBatData();
+if ~exist('readData','var')
+    [trajA, trajB, timeIndices] = loadBatData();
 
-% Pad the 2D trajectories to 3D ones.
-trajA = pad2DTrajectoryTo3D(trajA);
-trajB = pad2DTrajectoryTo3D(trajB);
+    % Pad the 2D trajectories to 3D ones.
+    trajA = pad2DTrajectoryTo3D(trajA);
+    trajB = pad2DTrajectoryTo3D(trajB);
+    readData = 1;
+end
+
+% Compute the distance terrain.
+distanceTerrain = matching.compute_distance_terrain(trajA,trajB);
+% Plot the distance terrain.
+visualise.plotDistanceTerrain(distanceTerrain);
 
 % Compute a Locally Correct Frechet Matchting.
 lcfMatching = matching.discrete_lcfm(trajA,trajB);
-% Plot the trjactories with its lcf matching.
+% Plot the trajectories with its lcf matching.
 visualise.plot_matching(trajA,trajB,lcfMatching);
