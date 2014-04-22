feea8d9977049fd7f2562feae8e94dfefa736e5f
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-22 13:24:24 +0200

    Function for padding 2D trajectories to 3D ones

---
+++ pad2DTrajectoryTo3D.m
@@ -0,0 +1,4 @@
+function [threeDtrajectory] = pad2DTrajectoryTo3D(twoDtrajectory)
+% Appends a new column in the trajectory for the "empty" third dimension with 0
+zeroColumn = zeros(length(twoDtrajectory),1)
+threeDtrajectory = [twoDtrajectory zeroColumn]
