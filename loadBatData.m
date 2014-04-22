6870398542deb6cd079a2dcd03a4830805336b3b
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-22 13:06:05 +0200

    Added comment and changed script into a matlab function

--- loadBatData.m
+++ loadBatData.m
@@ -1,3 +1,5 @@
+function [trajectoryA, trajectoryB, timeIndices] = loadBatData()
+% load bat data file as matrices A, B and a vector for the time indices
 pathToBatData = '/home/max/Documents/phd/bat_trajectory_data';
 filename = [pathToBatData, '/', 'bat_trajectory.txt'];
 delimiter = ' ';
@@ -6,5 +8,5 @@ headerLines = 1;
 batData = importdata(filename,delimiter,headerLines);
 dataMatrix = batData.data;
 timeIndices = dataMatrix(:,1);
-trajectoryA = dataMatrix(:, 2:3);
-trajectoryB = dataMatrix(:, 4:5);
+trajectoryA = dataMatrix(:,2:3);
+trajectoryB = dataMatrix(:,4:5);
