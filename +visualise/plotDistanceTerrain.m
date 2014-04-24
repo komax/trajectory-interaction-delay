2c7a4517c61c0f5088b297508000c6f51224f322
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-24 17:07:36 +0200

    Changed color map for distance terrain to hot (heated body color map)

--- +visualise/plotDistanceTerrain.m
+++ +visualise/plotDistanceTerrain.m
@@ -1,8 +1,9 @@
 function plotDistanceTerrain(distanceTerrain)
-% Plots a heat map in the distance terrain of the trajectories.
+% Plots a heated body map of the distance terrain of the trajectories.
 figure;
 hold on;
 imagesc(distanceTerrain);
+colormap(hot);
 axis tight;
 end
 
