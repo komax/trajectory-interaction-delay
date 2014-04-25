94907cd8cf2bcad7bc696117335dbf54cb1f5596
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-25 16:57:27 +0200

    Refactoring and made trajectories in the matching

--- src/frechet/Matching.java
+++ src/frechet/Matching.java
@@ -1,28 +1,27 @@
 package frechet;
 
-import java.lang.Math;
 import java.util.List;
 
 public class Matching {
     public int[] i;
     public int[] j;
-    public double[][] t1;
-    public double[][] t2;
+    private double[][] trajectory1;
+    private double[][] trajectory2;
 
-    public Matching(List<Node> path, double[][] t1, double[][] t2) {
-        i = new int[path.size()];
-        j = new int[path.size()];
-        this.t1 = t1;
-        this.t2 = t2;
+    public Matching(List<Node> path, double[][] trajectory1, double[][] trajectory2) {
+        this.i = new int[path.size()];
+        this.j = new int[path.size()];
+        this.trajectory1 = trajectory1;
+        this.trajectory2 = trajectory2;
 
         for (int p = 0; p < path.size(); p++) {
             Node node = path.get(p);
-//            double x1 = this.t1[node.i][0];
-//            double y1 = this.t1[node.i][1];
-//            double z1 = this.t1[node.i][2];
-//            double x2 = this.t2[node.j][0];
-//            double y2 = this.t2[node.j][1];
-//            double z2 = this.t2[node.j][2];
+//            double x1 = this.trajectory1[node.i][0];
+//            double y1 = this.trajectory1[node.i][1];
+//            double z1 = this.trajectory1[node.i][2];
+//            double x2 = this.trajectory2[node.j][0];
+//            double y2 = this.trajectory2[node.j][1];
+//            double z2 = this.trajectory2[node.j][2];
 //            double xDelta = x1 - x2;
 //            double yDelta = y1 - y2;
 //            double zDelta = z1 - z2;
@@ -43,16 +42,16 @@ public class Matching {
             results[match] = new double[10];
             idx1 = this.i[match];
             idx2 = this.j[match];
-            results[match][0] = t1[idx1][0]; // x1
-            results[match][1] = t1[idx1][1]; // y1
-            results[match][2] = t1[idx1][2]; // z1
-            results[match][3] = t1[idx1][3]; // frame1
-            results[match][4] = t1[idx1][4]; // bird_id1
-            results[match][5] = t2[idx2][0]; // x2
-            results[match][6] = t2[idx2][1]; // y2
-            results[match][7] = t2[idx2][2]; // z2
-            results[match][8] = t2[idx2][3]; // frame2
-            results[match][9] = t2[idx2][4]; // bird_id2
+            results[match][0] = trajectory1[idx1][0]; // x1
+            results[match][1] = trajectory1[idx1][1]; // y1
+            results[match][2] = trajectory1[idx1][2]; // z1
+            results[match][3] = trajectory1[idx1][3]; // frame1
+            results[match][4] = trajectory1[idx1][4]; // bird_id1
+            results[match][5] = trajectory2[idx2][0]; // x2
+            results[match][6] = trajectory2[idx2][1]; // y2
+            results[match][7] = trajectory2[idx2][2]; // z2
+            results[match][8] = trajectory2[idx2][3]; // frame2
+            results[match][9] = trajectory2[idx2][4]; // bird_id2
         }
 
         return results;
@@ -71,6 +70,14 @@ public class Matching {
         }
         builder.append("\n)");
         return builder.toString();
+    }
+
+    public double[][] getTrajectory1() {
+        return trajectory1;
+    }
+
+    public double[][] getTrajectory2() {
+        return trajectory2;
     }
 
 ///    /*
