d0962177e42f85df116a9321dc3e9e34c1e937cf
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-25 17:07:29 +0200

    Skeleton for plotting a matching

---
+++ src/visualization/MatchingPlot.java
@@ -0,0 +1,27 @@
+package visualization;
+
+import frechet.Matching;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by max on 25-4-14.
+ */
+public class MatchingPlot extends JPanel {
+    private final Matching matching;
+    private final double[][] trajectory1;
+    private final double[][] trajectory2;
+
+    public MatchingPlot(Matching matching) {
+        super(new BorderLayout());
+
+        // Store data to plot
+        this.matching = matching;
+        this.trajectory1 = matching.getTrajectory1();
+        this.trajectory2 = matching.getTrajectory2();
+
+        // TODO plot them
+        // TODO Add panel to plot by calling add()
+    }
+}
