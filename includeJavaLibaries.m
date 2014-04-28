5df2a5817c5f93cce51d886bf8d5ddd93a349653
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-28 16:43:00 +0200

    Changed matlab binding to java class files from netbeans

--- includeJavaLibaries.m
+++ includeJavaLibaries.m
@@ -2,6 +2,7 @@ function includeJavaLibaries()
 % Only include custom java classes to class path
 % if necessary
 if isempty(javaclasspath('-dynamic'))
-    javaaddpath bin;
+    %javaaddpath bin;
+    javaaddpath build/classes;
 end
 end
