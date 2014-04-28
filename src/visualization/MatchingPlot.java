6a9a8b216dd978e0113eb46febee5dc54d133bee
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-04-28 11:46:26 +0200

    Find ranges for plotting the matching

--- src/visualization/MatchingPlot.java
+++ src/visualization/MatchingPlot.java
@@ -13,6 +13,11 @@ public class MatchingPlot extends JPanel {
     private final double[][] trajectory1;
     private final double[][] trajectory2;
 
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+
     public MatchingPlot(Matching matching) {
         super(new BorderLayout());
 
@@ -21,8 +26,34 @@ public class MatchingPlot extends JPanel {
         this.trajectory1 = matching.getTrajectory1();
         this.trajectory2 = matching.getTrajectory2();
 
+        this.minX = Double.MAX_VALUE;
+        this.maxX = Double.MIN_VALUE;
+
+        this.minY = Double.MAX_VALUE;
+        this.maxY = Double.MIN_VALUE;
+
+        findMinAndMaxOn(trajectory1);
+        findMinAndMaxOn(trajectory2);
+
         // TODO plot them
         // TODO Add panel to plot by calling add()
+    }
+
+    private void findMinAndMaxOn(double[][] trajectory) {
+        for (int i=0; i<trajectory.length; i++) {
+            double xValue = trajectory[i][0];
+            if (minX > xValue) {
+                minX = xValue;
+            } else if (maxX < xValue) {
+                maxX = xValue;
+            }
+            double yValue = trajectory[i][1];
+            if (minY > yValue) {
+                minY = yValue;
+            } else if (maxY < yValue) {
+                maxY = yValue;
+            }
+        }
     }
 
     public Dimension getPreferredSize() {
