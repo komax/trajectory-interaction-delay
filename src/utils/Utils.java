53958b689b425e2824ad054cb2cffc1f7ee3a6c1
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-13 15:03:08 +0200

    Add of Utils class for some generic functions

---
+++ src/utils/Utils.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+import frechet.Matching;
+
+/**
+ *
+ * @author max
+ */
+public class Utils {
+    public static double[] delayWithEuclideanNorm(Matching matching) {
+        double[] delay = new double[matching.i.length];
+        for (int k=0; k<delay.length; k++) {
+            double[] pointI = matching.getTrajectory1()[matching.i[k]];
+            double[] pointJ = matching.getTrajectory2()[matching.j[k]];
+            delay[k] = euclideanDistance(pointI, pointJ);
+        }
+        return delay;
+    }
+    
+    private static double euclideanDistance(double[] pointA, double[] pointB) {
+        double summedDistance = 0.0;
+        for (int i=0; i<pointA.length; i++) {
+            summedDistance += pointA[i] * pointB[i];
+        }
+        return Math.sqrt(summedDistance);
+    }
+    
+}
