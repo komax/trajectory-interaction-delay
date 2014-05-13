cde47cf316e973ad61e147039d8cc8ada7df7666
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-13 16:50:13 +0200

    Launching facilities for matlab

--- src/visualization/VisualizationLauncher.java
+++ src/visualization/VisualizationLauncher.java
@@ -13,6 +13,7 @@ public class VisualizationLauncher {
     public static void main(String[] args) {
         Matching matching = MatchingReader.readMatching("batsMatching.dump");
         launchMatchingPlot(matching);
+        launchDelayPlot(matching);
     }
 
     public static void launchMatchingPlot(final Matching matching) {
@@ -23,11 +24,28 @@ public class VisualizationLauncher {
             }
         });
     }
+    
+    public static void launchDelayPlot(final Matching matching) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowDelayPlot(matching);
+            }
+        });
+    }
 
     private static void createAndShowMatchingPlot(Matching matching) {
         JFrame frame = new JFrame("Plotting Locally Correct Frechet Matching");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.add(new MatchingPlot(matching));
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    private static void createAndShowDelayPlot(Matching matching) {
+        JFrame frame = new JFrame("Plotting Normalized Delays");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(new DelayPlotPanel(matching));
         frame.pack();
         frame.setVisible(true);
     }
