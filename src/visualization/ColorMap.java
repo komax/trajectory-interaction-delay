81c9cd385bbf15306f2b0b5b877fca0e1216b7f3
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-19 10:42:52 +0200

    Factory for creating a blueish color map

--- src/visualization/ColorMap.java
+++ src/visualization/ColorMap.java
@@ -7,6 +7,7 @@
 package visualization;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,10 +19,28 @@ public class ColorMap {
     private final double maxValue;
     private final List<Color> colorSpectrum;
     
+    public static Color getColorFromRGB(int red, int green, int blue) {
+        return new Color(red, green, blue);
+    }
+    
     public static ColorMap createHeatedBodyColorMap(double minValue, double maxValue) {
         // TODO create colorSpectrum
         List<Color> heatedColorMap = null;
         return new ColorMap(minValue, maxValue, heatedColorMap);
+    }
+    
+    public static ColorMap createBlueColormap(double minValue, double maxValue) {
+        List<Color> colors = new ArrayList<>();
+        colors.add(getColorFromRGB(255, 247, 251));
+        colors.add(getColorFromRGB(236, 231, 242));
+        colors.add(getColorFromRGB(208, 209, 230));
+        colors.add(getColorFromRGB(166, 189, 219));
+        colors.add(getColorFromRGB(116, 169, 207));
+        colors.add(getColorFromRGB(54, 144, 192));
+        colors.add(getColorFromRGB(5, 112, 176));
+        colors.add(getColorFromRGB(4, 90, 141));
+        colors.add(getColorFromRGB(2, 56, 88));
+        return new ColorMap(minValue, maxValue, colors);
     }
 
     public ColorMap(double minValue, double maxValue, List<Color> colorSpectrum) {
