983c809294bb35a0f83af8c9210cd721023746ee
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-19 11:17:04 +0200

    Halfed the color spectrum for free space diagram

--- +visualise/plotMatchingInFreeSpace.m
+++ +visualise/plotMatchingInFreeSpace.m
@@ -5,9 +5,21 @@ figure;
 hold on;
 distanceTerrain = flipdim(distanceTerrain,1);
 imagesc(distanceTerrain);
-colormap(hot);
+heatedColorMap = colormap(hot);
+heatedColorMap
+myHeatedColorMap = zeros(length(heatedColorMap)/2,3);
+myHeatedColorMap(1) = heatedColorMap(1);
+for row = 2:length(heatedColorMap)
+    if mod(row,2)
+        mappedIndex = (row-1) / 2  + 1;
+        myHeatedColorMap(mappedIndex, :) = heatedColorMap(row, :);
+    end
+end
+myHeatedColorMap
+colormap(myHeatedColorMap);
 plot(javaMatchingObj.j,javaMatchingObj.i,'g');
 set(gca,'position',[0 0 1 1],'units','normalized');
+axis square;
 axis tight;
 axis off;
 end
