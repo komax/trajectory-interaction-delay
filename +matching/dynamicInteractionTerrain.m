5359bcc67dd432e6eac0fe8264cf691159ddd31c
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-26 19:00:17 +0200

    Using one script to run different types of experiments within matlab

--- +matching/dynamicInteractionTerrain.m
+++ +matching/dynamicInteractionTerrain.m
@@ -1,5 +1,6 @@
 function [distanceTerrain] = dynamicIterationTerrain(traject1,traject2,alpha)
 displacements = matching.computeDisplacement(traject1,traject2,alpha);
 attract = matching.computeHeading(traject1,traject2);
-distanceTerrain = displacements.*(attract);
+distract = 1 - attract;
+distanceTerrain = displacements.*(distract);
 end
--- runBatsExperiments.m
+++ runBatsExperiments.m
@@ -9,15 +9,30 @@ if ~readData
     readData = 1;
 end
 
+typeDistanceTerrain = 'directionalDistance';
+
 % Compute the distance terrain.
-distanceTerrain = matching.compute_distance_terrain(trajA,trajB);
+switch typeDistanceTerrain
+    case 'normal'
+        distanceTerrain = matching.compute_distance_terrain(trajA,trajB);
+        matchingName = 'batsMatching.dump';
+    case 'directionalDistance'
+        distanceTerrain = matching.directionalDistanceTerrain(trajA,trajB);
+        matchingName = 'batsMatchingdirectionalDistance.dump';
+    case 'dynamicIteraction'
+        alpha = 2;
+        distanceTerrain = matching.dynamicInteractionTerrain(trajA,trajB,alpha);
+        matchingName = 'batsMatchingDynamicIteraction.dump';
+    otherwise
+        error('Cannot handle this choice');
+end
 % Plot the distance terrain.
 visualise.plotDistanceTerrain(distanceTerrain);
 
 % Compute a Locally Correct Frechet Matchting.
 lcfMatching = matching.discrete_lcfm(trajA,trajB,distanceTerrain);
 % Save matching on disk.
-matching.writeMatching(lcfMatching,'batsMatching.dump');
+matching.writeMatching(lcfMatching,matchingName);
 % Plot the trajectories with its lcf matching.
 visualise.plot_matching(trajA,trajB,lcfMatching);
 % Plot free space with its matching.
