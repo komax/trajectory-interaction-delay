6a082a4a9dea0d594820afc604e718f5a4dc4111
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-26 11:57:35 +0200

    More functions to compute the actual heading information.

--- +matching/computeHeading.m
+++ +matching/computeHeading.m
@@ -1,6 +1,12 @@
 function [headingMatrix] = computeHeading(traject1,traject2)
 headingMatrix = zeros(length(traject1),length(traject2));
-
+for i = 1:length(traject1)
+    for j = 1:length(traject2)
+        angleI = headingOnTrajectory(traject1, i);
+        angleJ = headingOnTrajectory(traject2, j);
+        headingMatrix(i,j) = attraction(angleI, angleJ);
+    end
+end
 headingMatrix = flipdim(headingMatrix, 1);
 end
 
@@ -14,3 +20,23 @@ projectedPoint = [p(1), successorP(2:)];
 headingValue = 0;
 end
 
+function [headingValue] = headingOnTrajectory(traject, timestamp)
+if timestamp == length(traject)
+    headingValue = inf;
+else
+    headingValue = headingAngle(traject(timestamp), traject(timestamp));
+end
+end
+
+function [attractionValue] = attraction(headingA, headingB)
+if headingA == inf
+    if headingB == inf
+        attractionValue = 1;
+    else
+        attractionValue = 0;
+    end
+elseif headingB == inf
+    attractionValue = 0;
+else
+    attractionValue = cos(headingA - headingB);
+end
