44a1f4dfe66061bcf13e6dbcc6edf10f272b0622
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-05-28 17:52:56 +0200

    Selecting a norm by a number

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package utils;
 
 import frechet.Matching;
@@ -13,6 +12,7 @@ import frechet.Matching;
  * @author max
  */
 public class Utils {
+
     public static final DistanceNorm EuclideanDistance = new AbstractPNorm() {
 
         @Override
@@ -20,15 +20,15 @@ public class Utils {
             return 2;
         }
     };
-    
+
     public static final DistanceNorm L1Distance = new AbstractPNorm() {
-        
+
         @Override
         public int getOrder() {
             return 1;
         }
     };
-    
+
     public static final DistanceNorm LInfDistance = new DistanceNorm() {
 
         @Override
@@ -41,16 +41,29 @@ public class Utils {
             return maxDistance;
         }
     };
-    
+
+    public static DistanceNorm selectDistanceNorm(int order) {
+        switch (order) {
+            case 1:
+                return L1Distance;
+            case 2:
+                return EuclideanDistance;
+            case Integer.MAX_VALUE:
+                return LInfDistance;
+            default:
+                throw new RuntimeException("order=" + order + " is not a valid norm");
+        }
+    }
+
     public static int findMatchingIndex(Matching matching, int i, int j) {
-        for (int k=0; k<matching.i.length; k++) {
+        for (int k = 0; k < matching.i.length; k++) {
             if (matching.i[k] == i && matching.j[k] == j) {
                 return k;
             }
         }
         return -1;
     }
-    
+
     public static double[] normalizedDelay(double[] delays) {
         double maxDelay = Double.MIN_VALUE;
         double[] normalizedDelays = new double[delays.length];
@@ -59,48 +72,48 @@ public class Utils {
                 maxDelay = delay;
             }
         }
-        
-        for (int i=0; i<delays.length; i++) {
+
+        for (int i = 0; i < delays.length; i++) {
             normalizedDelays[i] = delays[i] / maxDelay;
         }
         return normalizedDelays;
     }
-    
+
     public static boolean[] trajectroy1IsAhead(Matching matching) {
         boolean[] traject1IsAhead = new boolean[matching.i.length];
-        for (int k=0; k<traject1IsAhead.length; k++) {
+        for (int k = 0; k < traject1IsAhead.length; k++) {
             traject1IsAhead[k] = matching.i[k] > matching.j[k];
         }
         return traject1IsAhead;
     }
-    
+
     public static boolean[] trajectroy2IsAhead(Matching matching) {
         boolean[] traject2IsAhead = new boolean[matching.i.length];
-        for (int k=0; k<traject2IsAhead.length; k++) {
+        for (int k = 0; k < traject2IsAhead.length; k++) {
             traject2IsAhead[k] = matching.i[k] < matching.j[k];
         }
         return traject2IsAhead;
     }
-    
+
     public static double[] distancesOnMatching(Matching matching, DistanceNorm distance) {
         double[] delay = new double[matching.i.length];
         double[][] traject1 = matching.getTrajectory1();
         double[][] traject2 = matching.getTrajectory2();
-        for (int k=0; k<delay.length; k++) {
+        for (int k = 0; k < delay.length; k++) {
             double[] pointI = traject1[matching.i[k]];
             double[] pointJ = traject2[matching.j[k]];
             delay[k] = distance.distance(pointI, pointJ);
         }
         return delay;
     }
-    
+
     public static int[] delayInTimestamps(Matching matching) {
         int[] delays = new int[matching.i.length];
-        for (int k=0; k<delays.length; k++) {
+        for (int k = 0; k < delays.length; k++) {
             int diff = Math.abs(matching.i[k] - matching.j[k]);
             delays[k] = diff;
         }
         return delays;
     }
-    
+
 }
