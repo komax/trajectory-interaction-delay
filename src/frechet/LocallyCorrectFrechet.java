c1308941a9511370e5245094750c517d48e6ef0f
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-06-03 11:45:28 +0200

    Code formatting and clean up

--- src/frechet/LocallyCorrectFrechet.java
+++ src/frechet/LocallyCorrectFrechet.java
@@ -1,4 +1,5 @@
 package frechet;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -6,28 +7,26 @@ import java.util.List;
 public class LocallyCorrectFrechet {
 
     public static Matching compute(double[][] distanceTerrain, double[][] traject1, double[][] traject2,
-                                   int numRows, int numColumns) {
+            int numRows, int numColumns) {
         if ((distanceTerrain[0].length != numColumns) || (distanceTerrain.length != numRows)) {
             throw new RuntimeException("Size of grid and size of trajectories disagree\n");
         }
         PathTree tree = new PathTree(distanceTerrain, numRows, numColumns);
 
-        
-        for (int i = 1; i < numRows ; i++ ) {
-            tree.add(i,0);
+        for (int i = 1; i < numRows; i++) {
+            tree.add(i, 0);
         }
-        for (int j = 1; j < numColumns ; j++) {
-            tree.add(0,j);
+        for (int j = 1; j < numColumns; j++) {
+            tree.add(0, j);
         }
-        for (int i = 1; i < numRows ; i++) {
-            for (int j = 1; j < numColumns ; j++) {
-                tree.add(i,j);
+        for (int i = 1; i < numRows; i++) {
+            for (int j = 1; j < numColumns; j++) {
+                tree.add(i, j);
             }
         }
 
-
         List<Node> path = new ArrayList<Node>();
-        Node node = tree.grid[numRows-1][numColumns-1];
+        Node node = tree.grid[numRows - 1][numColumns - 1];
         while (node != null) {
             path.add(node);
             node = node.parent;
