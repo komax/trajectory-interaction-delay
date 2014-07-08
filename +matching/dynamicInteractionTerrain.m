e5d817158eea8a8be5233ab1ead35b7e723fde3d
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-07-08 14:47:50 +0200

    Correct transformation from similarity measure to semidistance

--- +matching/dynamicInteractionTerrain.m
+++ +matching/dynamicInteractionTerrain.m
@@ -1,6 +1,5 @@
 function [distanceTerrain] = dynamicIterationTerrain(traject1,traject2,normNumber,alpha)
 displacements = matching.computeDisplacement(traject1,traject2,normNumber,alpha);
 attract = matching.computeHeading(traject1,traject2);
-distract = 1 - attract;
-distanceTerrain = displacements.*(distract);
+distanceTerrain = 1 - displacements.*(attract);
 end
