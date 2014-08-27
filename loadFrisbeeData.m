756303f63259d60814fabc6b54f8a745d5ce2195
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-08-27 16:13:36 +0200

    Loading the Ultimate Frisbee data set into matlab

--- loadFrisbeeData.m
+++ loadFrisbeeData.m
@@ -1,13 +1,13 @@
 function [trajectoryA, trajectoryB] = loadFrisbeeData()
 % load frisbee data file
 pathToFrisbeeData = '/home/max/Documents/phd/ultimate_frisbee_data';
-delimiter = ',';
+delimiter = ' ';
 headerLines = 1;
-filename = [pathToFrisbeeData, '/', 'player1_TGISpaper.csv'];
+filename = [pathToFrisbeeData, '/', 'player1_cleaned.csv'];
 player1Data = importdata(filename,delimiter,headerLines);
 dataMatrix = player1Data.data;
-trajectoryA = dataMatrix(:,2:3);
-filename = [pathToFrisbeeData, '/', 'player2_TGISpaper.csv'];
+trajectoryA = dataMatrix(:,1:2);
+filename = [pathToFrisbeeData, '/', 'player2_cleaned.csv'];
 player2Data = importdata(filename,delimiter,headerLines);
 dataMatrix = player2Data.data;
-trajectoryB = dataMatrix(:,2:3);
+trajectoryB = dataMatrix(:,1:2);
