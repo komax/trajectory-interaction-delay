6669f83b497d856916847d1020fc6ef39e6bed77
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-08-27 17:04:29 +0200

    Not working script on running a bunch of bats experiments

---
+++ runBatsExperiments.m
@@ -0,0 +1,10 @@
+delaySpaces = {'normal' 'directionalDistance' 'dynamicInteraction' 'dynamicDistance'};
+normes = [1 2 Inf];
+
+for i = 1:length(delaySpaces)
+    currentDelaySpace = delaySpaces(i);
+    for j = 1:length(normes)
+        currentNorm = normes(j);
+        runBatsExperiment(currentDelaySpace,currentNorm);
+    end
+end
