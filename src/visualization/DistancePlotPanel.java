846229eb26ac22f5617457816884940e01478002
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-09-09 15:53:52 +0200

    Add an axis to the distance plot

--- src/visualization/DistancePlotPanel.java
+++ src/visualization/DistancePlotPanel.java
@@ -12,8 +12,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 import utils.DistanceNorm;
 import utils.Utils;
+import static visualization.GenericPlottingPanel.roundDouble;
 
 /**
  *
@@ -109,6 +111,21 @@ public final class DistancePlotPanel extends GenericPlottingPanel {
         int maxY = roundDouble(maxPoint.y);
         String maxDistanceString = String.format("%.3f", maxDistance);
         g.drawString(maxDistanceString, 0, maxY);
+        Stroke oldStroke = g2.getStroke();
+        float dash1[] = {10.0f};
+        BasicStroke dashed = new BasicStroke(1.0f,
+                        BasicStroke.CAP_BUTT,
+                        BasicStroke.JOIN_MITER,
+                        10.0f, dash1, 0.0f);
+        g2.setStroke(dashed);
+        Point2D origin = new Point2D(0, maxDistanceNormalized);
+        Point2D drawableOrigin = cartesianToPanelPoint(origin);
+        int xCoord = roundDouble(drawableOrigin.x);
+        int yCoord = roundDouble(drawableOrigin.y);
+        g.drawLine(xCoord, yCoord, getWidth(), yCoord);
+
+        // Restore old stroke style.
+        g2.setStroke(oldStroke);
         
         g.setColor(Color.white);
         Point2D minPoint = cartesianToPanelPoint(new Point2D(0, minDistanceNormalized));
@@ -121,8 +138,8 @@ public final class DistancePlotPanel extends GenericPlottingPanel {
             g.setColor(Color.black);
             Point2D selectedPoint = new Point2D(selectedIndex, normalizedDistances[selectedIndex]);
             Point2D drawablePoint = cartesianToPanelPoint(selectedPoint);
-            int xCoord = roundDouble(drawablePoint.x);
-            int yCoord = roundDouble(drawablePoint.y);
+            xCoord = roundDouble(drawablePoint.x);
+            yCoord = roundDouble(drawablePoint.y);
             
             g.drawLine(axisWidth(), yCoord, xCoord, yCoord);
             g.drawLine(xCoord, axisHeight(), xCoord, getHeight());
