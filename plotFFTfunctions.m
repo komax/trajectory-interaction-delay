b5d180c0f99789ce2a76a6b75fe35f63955eb7e7
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-09-18 14:15:47 +0200

    Plots for the fft approaches and their approxs

--- plotFFTfunctions.m
+++ plotFFTfunctions.m
@@ -1,22 +1,44 @@
 x = linspace(0.0,1.0);
 y = linspace(0.0,1.0);
 
-alpha = 3;
-displacements = zeros(length(x),length(y));
-for i = 1:length(x)
+alpha = 1;
+displacementsBigAlpha = zeros(length(x),length(y));
+for k = 1:length(x)
     for j = 1:length(y)
-        displacements(i,j) = 1. - (abs(x(i)-y(j))/(x(i)+y(j)))^alpha;
+        displacementsBigAlpha(k,j) = 1. - (abs(x(k)-y(j))/(x(k)+y(j)))^alpha;
     end
 end
-mesh(x,y,displacements);
+figure;
+mesh(x,y,displacementsBigAlpha);
 
 complexX = complex(x);
 complexY = complex(y);
 displacementApproxs = zeros(length(x),length(y));
-alpha = 0.1;
-for i = 1:length(x)
+alpha = 1;
+for k = 1:length(x)
     for j = 1:length(y)
-        displacementApproxs(i,j) = real(exp(log(complexY(j) + alpha) - log(complexX(i) + alpha)* i * pi / 2));
+        displacementApproxs(k,j) = real(exp(log(complexY(j) + alpha) - log(complexX(k) + alpha)* i * pi / 2));
     end
 end
+figure;
 mesh(x,y,displacementApproxs);
+
+alpha = 0.5;
+displacementsSmallAlpha = zeros(length(x),length(y));
+for k = 1:length(x)
+    for j = 1:length(y)
+        displacementsSmallAlpha(k,j) = 1. - (abs(x(k)-y(j))/(x(k)+y(j)))^alpha;
+    end
+end
+figure;
+mesh(x,y,displacementsSmallAlpha);
+
+displacementApproxsSmallAlpha = zeros(length(x),length(y));
+alpha = 0.5;
+for k = 1:length(x)
+    for j = 1:length(y)
+        displacementApproxsSmallAlpha(k,j) = real(complexY(j)^0.1 * complexX(k)^0.1 * exp(log(complexY(j) + alpha) - log(complexX(k) + alpha)* i * pi / 2));
+    end
+end
+figure;
+mesh(x,y,displacementApproxsSmallAlpha);
