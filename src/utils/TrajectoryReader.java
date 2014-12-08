667847cd22fb062fa9fa387e0a2ad3c041e69120
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-12-08 16:49:39 +0100

    Adding a trajectory reader from a text file (not tested yet)

---
+++ src/utils/TrajectoryReader.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author max
+ */
+public class TrajectoryReader {
+    private double[][] trajectory1;
+    private double[][] trajectory2;
+    private final BufferedReader inputReader;
+    
+    public TrajectoryReader(String fileName) throws Exception {
+        this.inputReader = new BufferedReader(new FileReader(fileName));
+        
+        // Read content of the file into the fields.
+        readTrajectories();
+    }
+    
+    private void readTrajectories() throws IOException {
+        List<List<Double>> traject1 = new ArrayList<>();
+        List<List<Double>> traject2 = new ArrayList<>();
+        
+        // Skip the first line.
+        inputReader.readLine();
+        
+        String currentLine = "";
+        
+        // Read the lines and their data points.
+        while ((currentLine = inputReader.readLine()) != null) {
+            String[] lineElements = currentLine.split("\\s+");
+            List<Double> pointsTraject1 = new ArrayList<>();
+            List<Double> pointsTraject2 = new ArrayList<>();
+            pointsTraject1.add(Double.parseDouble(lineElements[1]));
+            pointsTraject1.add(Double.parseDouble(lineElements[2]));
+            traject1.add(pointsTraject1);
+            pointsTraject2.add(Double.parseDouble(lineElements[3]));
+            pointsTraject2.add(Double.parseDouble(lineElements[4]));
+            traject2.add(pointsTraject2);
+        }
+        
+        int dimensions = 2;
+        this.trajectory1 = convertTrajectoryRecursively(traject1, dimensions);
+        this.trajectory2 = convertTrajectoryRecursively(traject2, dimensions);
+    }
+    
+    private double[][] convertTrajectoryRecursively(List<List<Double>> traject, int dimensions) {
+        int length = traject.size();
+        double[][] result = new double[length][dimensions];
+        
+        return result;
+    }
+    
+    public double[][] getTrajectory1() {
+        return trajectory1;
+    }
+    
+    public double[][] getTrajectory2() {
+        return trajectory2;
+    }
+    
+}
