993e60f3746dfdb16f1e31d639193a36d6d0c8fb
Maximilian Konzack <maximilian.konzack@gmail.com>
2014-12-10 17:14:28 +0100

    Adding some fields to the delayspace class

--- src/delayspace/DelaySpace.java
+++ src/delayspace/DelaySpace.java
@@ -5,6 +5,8 @@
  */
 package delayspace;
 
+import utils.distance.DistanceNorm;
+import utils.distance.DistanceNormFactory;
 import utils.distance.DistanceNormType;
 
 /**
@@ -13,12 +15,23 @@ import utils.distance.DistanceNormType;
  */
 public class DelaySpace {
     
+    private final DistanceNorm distance;
+    private final DelaySpaceType delaySpaceType;
+    private final double[][] delaySpace;
+    
     public DelaySpace(double[][] trajectory1, double[][] trajectory2, DelaySpaceType delayType, DistanceNormType normType) {
+        this.distance = DistanceNormFactory.getDistanceNorm(normType);
+        this.delaySpaceType = delayType;
         
+        int lengthTraject1 = trajectory1.length;
+        int lengthTraject2 = trajectory2.length;
+        
+        this.delaySpace = new double[lengthTraject1][lengthTraject2];
+        // TODO Compute the values for the delay space.
     }
     
     public double[][] getDelaySpace() {
-        return null;
+        return delaySpace;
     }
     
 }
