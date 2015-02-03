c6a07b8bac27e488ae68ea82f1001d03c54fd211
Maximilian Konzack <m.p.konzack@tue.nl>
2015-02-03 15:35:33 +0100

    Use the summer color map and discard the log function approximation.

--- plotFFTfunctions.m
+++ plotFFTfunctions.m
@@ -1,43 +1,37 @@
 x = linspace(0.0,1.0);
 y = linspace(0.0,1.0);
 
-alpha = 1.0;
-displacementsBigAlpha = zeros(length(x),length(y));
+alpha = 2;
+displacements = zeros(length(x),length(y));
 for k = 1:length(x)
     for j = 1:length(y)
         displacementsBigAlpha(k,j) = 1. - (abs(x(k)-y(j))/(x(k)+y(j)))^alpha;
     end
 end
 figure;
+% Use a different color map.
+colormap(summer);
 mesh(x,y,displacementsBigAlpha);
 
 % This function does not fit nicely. Drop it
 complexX = complex(x);
 complexY = complex(y);
+%displacementApproxs = zeros(length(x),length(y));
+%for k = 1:length(x)
+%    for j = 1:length(y)
+%        displacementApproxs(k,j) = real(exp(log((complexY(j) + alpha) - log(complexX(k) + alpha))* i * pi / 2));
+%    end
+%end
+%figure;
+%mesh(x,y,displacementApproxs);
+
 displacementApproxs = zeros(length(x),length(y));
 for k = 1:length(x)
     for j = 1:length(y)
-        displacementApproxs(k,j) = real(exp(log((complexY(j) + alpha) - log(complexX(k) + alpha))* i * pi / 2));
+        displacementApproxs(k,j) = real(complexY(j)^0.1 * complexX(k)^0.1 * exp((log(complexY(j) + alpha) - log(complexX(k) + alpha)) * i * pi / 2));
     end
 end
 figure;
-mesh(x,y,displacementApproxs);
-
-alpha = 0.999;
-displacementsSmallAlpha = zeros(length(x),length(y));
-for k = 1:length(x)
-    for j = 1:length(y)
-        displacementsSmallAlpha(k,j) = 1. - (abs(x(k)-y(j))/(x(k)+y(j)))^alpha;
-    end
-end
-figure;
-mesh(x,y,displacementsSmallAlpha);
-
-displacementApproxsSmallAlpha = zeros(length(x),length(y));
-for k = 1:length(x)
-    for j = 1:length(y)
-        displacementApproxsSmallAlpha(k,j) = real(complexY(j)^0.1 * complexX(k)^0.1 * exp((log(complexY(j) + alpha) - log(complexX(k) + alpha)) * i * pi / 2));
-    end
-end
-figure;
+% Use a different color map.
+colormap(summer);
 mesh(x,y,displacementApproxsSmallAlpha);
