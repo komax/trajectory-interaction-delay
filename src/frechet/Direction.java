108ee60019d259ba1388f461d11965d00b98c7bd
Maximilian Konzack <m.p.konzack@tue.nl>
2015-02-03 13:07:59 +0100

    Add of Direction enum and a prototype for storing shortcuts

---
+++ src/frechet/Direction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frechet;
+
+/**
+ *
+ * @author max
+ */
+public enum Direction {
+    UP, RIGHT, DIAG_UP, DIAG_RIGHT;
+}
---
+++ src/frechet/Shortcut.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frechet;
+
+/**
+ *
+ * @author max
+ */
+public class Shortcut {
+    private final Node from;
+    private final Node to;
+    private final double maxValue;
+    private final Direction incomingDirection;
+    
+    public Shortcut(Node from, Node to, double maxValue, Direction incomingDirection) {
+        this.from = from;
+        this.to = to;
+        this.maxValue = maxValue;
+        this.incomingDirection = incomingDirection;
+    }
+    
+}
