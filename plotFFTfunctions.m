3f8d3104d1bf9ed44a24318e379dae1b89237ebe
Maximilian Konzack <m.p.konzack@tue.nl>
2015-02-04 11:34:48 +0100

    Split of the adjustment functions

--- plotFFTfunctions.m
+++ plotFFTfunctions.m
@@ -16,22 +16,32 @@ mesh(x,y,displacementsBigAlpha);
 % This function does not fit nicely. Drop it
 complexX = complex(x);
 complexY = complex(y);
-%displacementApproxs = zeros(length(x),length(y));
-%for k = 1:length(x)
-%    for j = 1:length(y)
-%        displacementApproxs(k,j) = real(exp(log((complexY(j) + alpha) - log(complexX(k) + alpha))* i * pi / 2));
-%    end
-%end
-%figure;
-%mesh(x,y,displacementApproxs);
+displacementApproxs = zeros(length(x),length(y));
+for k = 1:length(x)
+    for j = 1:length(y)
+        fx(k) = 1;
+        fy(j) = 1;
+        gx(k) = log(complexX(k) + alpha);
+        gy(j) = log(complexY(j) + alpha);
+        displacementApproxs(k,j) = real(fx(k)*fy(j)*exp((gy(j)-gx(k))*i*pi/2));
+        %displacementApproxs(k,j) = real(exp(log((complexY(j) + alpha) - log(complexX(k) + alpha))* i * pi / 2));
+    end
+end
+figure;
+mesh(x,y,displacementApproxs);
 
 displacementApproxs = zeros(length(x),length(y));
 for k = 1:length(x)
     for j = 1:length(y)
-        displacementApproxs(k,j) = real(complexY(j)^0.1 * complexX(k)^0.1 * exp((log(complexY(j) + alpha) - log(complexX(k) + alpha)) * i * pi / 2));
+        fx(k) = complexX(k)^0.1;
+        fy(j) = complexY(j)^0.1;
+        gx(k) = log(complexX(k)^2 + alpha);
+        gy(j) = log(complexY(j)^2 + alpha);
+        displacementApproxs(k,j) = real(fx(k)*fy(j)*exp((gy(j)-gx(k))*i*pi/2));
+        %displacementApproxs(k,j) = real(complexY(j)^0.1 * complexX(k)^0.1 * exp((log(complexY(j) + alpha) - log(complexX(k) + alpha)) * i * pi / 2));
     end
 end
 figure;
 % Use a different color map.
 colormap(summer);
-mesh(x,y,displacementApproxsSmallAlpha);
+mesh(x,y,displacementApproxs);
