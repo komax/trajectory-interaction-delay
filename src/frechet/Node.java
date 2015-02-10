515bcafc9fd8bfc40e7a71f0d4d8f88e0c529698
Maximilian Konzack <m.p.konzack@tue.nl>
2015-02-10 10:27:00 +0100

    Deleted legacy code

--- src/frechet/Node.java
+++ src/frechet/Node.java
@@ -1,12 +1,9 @@
 package frechet;
 
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
-public class Node implements Comparable<Node> {
+public class Node {
 
     private final int i, j;
     private final double value;
@@ -74,53 +71,6 @@ public class Node implements Comparable<Node> {
     public boolean isDead() {
         return !isRoot() && outdegree() == 0;
     }
-    
-    @Deprecated // TODO delete, since it does not use shortcuts
-    public LinkedHashSet<Node> pathToRoot() {
-        Node temp = this;
-        LinkedHashSet<Node> path = new LinkedHashSet<Node>();
-        while (temp != null) {
-            path.add(temp);
-            temp = temp.parent;
-        }
-        return path;
-    }
-    
-    @Deprecated // TODO delete, as it doese not consider shortcuts
-    public LinkedHashSet<Node> pathTo(Node ancestor) {
-        Node currentNode = this;
-        LinkedHashSet<Node> path = new LinkedHashSet<>();
-        while (currentNode != null) {
-            if (currentNode.equals(ancestor)) {
-                break;
-            }
-            path.add(currentNode);
-            currentNode = currentNode.parent;
-        }
-        return path;
-    }
-    
-    @Deprecated // FIXME delete this method
-    public double maxValueOnPathTo(Node ancestor) {
-        // FIXME Use shortcuts.
-        double maxVal = Double.MIN_VALUE;
-        for (Node currentNode : pathTo(ancestor)) {
-            if (currentNode.value > maxVal) {
-                maxVal = currentNode.value;
-            }
-        }
-        return maxVal;
-    }
-
-    @Deprecated
-    public Node nearestCommonAncestor(Node other) {
-        // FIXME more efficient implementation.
-        LinkedHashSet<Node> path1 = this.pathToRoot();
-        Set<Node> path2 = other.pathToRoot();
-        path1.retainAll(path2);
-        Iterator<Node> it = path1.iterator();
-        return it.next();
-    }
 
     @Override
     public boolean equals(Object obj) {
@@ -133,18 +83,6 @@ public class Node implements Comparable<Node> {
 
         Node rhs = (Node) obj;
         return (this.i == rhs.i) && (this.j == rhs.j);
-    }
-
-    @Deprecated // FXIME this check method ist not required anymore
-    public int compareTo(Node rhs) {
-        // if this < obj return -1
-        if (rhs == this) {
-            return 0;
-        } else if (rhs == null) {
-            throw new NullPointerException("Node is null: " + rhs);
-        } else {
-            return Integer.valueOf(i + j).compareTo(Integer.valueOf(rhs.i + rhs.j));
-        }
     }
 
     @Override
