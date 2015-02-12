8f658a2401fd10c8f16b329c3cf2f8e6b96dd359
Maximilian Konzack <m.p.konzack@tue.nl>
2015-02-12 15:12:55 +0100

    Adding edgecursor and dropping image painting

--- src/visualization/EdgeCursor.java
+++ src/visualization/EdgeCursor.java
@@ -28,4 +28,29 @@ public class EdgeCursor {
         return indexTrajB;
     }
     
+    public boolean isTrajAAhead() {
+        if (this == INVALID_CURSOR) {
+            return false;
+        } else {
+            return indexTrajA > indexTrajB;
+        }
+    }
+    
+    public boolean isTrajBAhead() {
+        if (this == INVALID_CURSOR) {
+            return false;
+        } else {
+            return indexTrajB > indexTrajA;
+        }
+    }
+    
+    public boolean isDelayLargerOrEqualThan(int threshold) {
+        int delay = Math.abs(indexTrajA - indexTrajB);
+        return delay >= threshold;
+    }
+    
+    public boolean isValid() {
+        return this != INVALID_CURSOR;
+    }
+    
 }
