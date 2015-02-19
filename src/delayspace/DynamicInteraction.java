708d8b7b19d56cb45b6c22d3422b33a8e8c6ba1b
Maximilian Konzack <m.p.konzack@tue.nl>
2015-02-19 15:24:45 +0100

    Fixing the computation of the dynamic interaction

--- src/delayspace/DynamicInteraction.java
+++ src/delayspace/DynamicInteraction.java
@@ -10,21 +10,23 @@ package delayspace;
  * @author max
  */
 public final class DynamicInteraction extends DelaySpace {
-    private final double[][] displacements;
-    private final double[][] headingDelaySpace;
+    private final DelaySpace displacements;
+    private final DelaySpace headingDelaySpace;
 
     public DynamicInteraction(double[][] trajectory1, double[][] trajectory2, double alpha) {
         super(trajectory1, trajectory2);
         
-        this.displacements = (new Displacement(trajectory1, trajectory2, alpha)).delaySpace;
-        this.headingDelaySpace = (new Heading(trajectory1, trajectory2)).delaySpace;
+        this.displacements = new Displacement(trajectory1, trajectory2, alpha);
+        this.headingDelaySpace = new Heading(trajectory1, trajectory2);
     }
 
     @Override
     protected void computeDelaySpace() {
+        displacements.computeDelaySpace();
+        headingDelaySpace.computeDelaySpace();
         for (int i = 0; i < trajectory1.length; i++) {
             for (int j = 0; j < trajectory2.length; j++) {
-                double dynamicInteractionValue = 1.0 - (displacements[i][j] * (headingDelaySpace[i][j] - 1.0));
+                double dynamicInteractionValue = 1.0 - (displacements.get(i, j) * (headingDelaySpace.get(i, j) - 1.0));
                 delaySpace[i][j] = dynamicInteractionValue;
             }
         }
