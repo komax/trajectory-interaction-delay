603efd227d498a634de2a4aeb34f082dbe54a34e
Maximilian Konzack <m.p.konzack@tue.nl>
2015-03-10 13:52:25 +0100

    Adding a trajectory class to model a discrete trajectory

---
+++ src/utils/Trajectory.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author max
+ */
+public class Trajectory implements Iterable<double[]> {
+    private final List<double[]> entries;
+    
+    public Trajectory() {
+        this.entries = new ArrayList<>();
+    }
+    
+    public void addPoint(double[] point) {
+        this.entries.add(point);
+    }
+    
+    public double[] getPoint(int index) {
+        return entries.get(index);
+    }
+    
+    public DoublePoint2D getPointObject(int index) {
+        double[] point = getPoint(index);
+        return new DoublePoint2D(point[0], point[1]);
+    }
+
+    @Override
+    public Iterator<double[]> iterator() {
+        return entries.iterator();
+    }
+    
+    public int length() {
+        return entries.size();
+    }
+}
