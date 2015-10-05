4a1709a922a40320a3bb309cbf33a4dcad2759e0
Maximilian Konzack <m.p.konzack@tue.nl>
2015-10-05 13:27:35 +0200

    Read a triplet of trajectories

---
+++ src/utils/TripletTrajectoryReader.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author max
+ */
+public class TripletTrajectoryReader {
+    private Trajectory trajectory1;
+    private Trajectory trajectory2;
+    private Trajectory trajectory3;
+    private final BufferedReader inputReader;
+    
+    public static TripletTrajectoryReader createTrajectoryReader(String fileName, boolean skipHeader) throws Exception {
+        return new TripletTrajectoryReader(fileName, skipHeader);
+    }
+    
+    public TripletTrajectoryReader(String fileName, boolean skipHeader) throws Exception {
+        this.inputReader = new BufferedReader(new FileReader(fileName));
+        
+        // Read content of the file into the fields.
+        readTrajectories(skipHeader);
+    }
+    
+    private void readTrajectories(boolean skipHeader) throws IOException {
+        this.trajectory1 = new Trajectory();
+        this.trajectory2 = new Trajectory();
+        this.trajectory3 = new Trajectory();
+        
+        if (skipHeader) {
+            // Skip the first line.
+            inputReader.readLine();
+        }
+        
+        String currentLine = "";
+        
+        // Read the lines and their data points.
+        while ((currentLine = inputReader.readLine()) != null) {
+            String[] lineElements = currentLine.split("\\s+");
+            double[] pointsTraject1 = new double[2];
+            double[] pointsTraject2 = new double[2];
+            double[] pointsTraject3 = new double[2];
+            if (lineElements[0].equals("")) {
+//                System.err.println(lineElements[1] + "|" + lineElements[2] + "|" + lineElements[3] + "|" + lineElements[4] + "|" + lineElements[5]);
+                pointsTraject1[0] = Double.parseDouble(lineElements[2]);
+                pointsTraject1[1] = Double.parseDouble(lineElements[3]);
+                pointsTraject2[0] = Double.parseDouble(lineElements[4]);
+                pointsTraject2[1] = Double.parseDouble(lineElements[5]);
+                pointsTraject3[0] = Double.parseDouble(lineElements[6]);
+                pointsTraject3[1] = Double.parseDouble(lineElements[7]);
+            } else {
+//                System.err.println(lineElements[0] + "|" + lineElements[1] + "|" + lineElements[2] + "|" + lineElements[3] + "|" + lineElements[4]);
+                pointsTraject1[0] = Double.parseDouble(lineElements[1]);
+                pointsTraject1[1] = Double.parseDouble(lineElements[2]);
+                pointsTraject2[0] = Double.parseDouble(lineElements[3]);
+                pointsTraject2[1] = Double.parseDouble(lineElements[4]);
+                pointsTraject3[0] = Double.parseDouble(lineElements[5]);
+                pointsTraject3[1] = Double.parseDouble(lineElements[6]);
+            }
+            trajectory1.addPoint(pointsTraject1);
+            trajectory2.addPoint(pointsTraject2);
+            trajectory3.addPoint(pointsTraject3);
+        }
+    }
+    
+    private double[][] convertTrajectoryRecursively(List<double[]> traject, int dimensions) {
+        int length = traject.size();
+        double[][] result = new double[length][dimensions];
+        
+        for (int i = 0; i < length; i++) {
+           double[] pointTraject = traject.get(i);
+           result[i] = pointTraject;
+        }
+        
+        return result;
+    }
+    
+    public Trajectory getTrajectory1() {
+        return trajectory1;
+    }
+    
+    public Trajectory getTrajectory2() {
+        return trajectory2;
+    }
+    
+    public Trajectory getTrajectory3() {
+        return trajectory3;
+    }
+    
+}
