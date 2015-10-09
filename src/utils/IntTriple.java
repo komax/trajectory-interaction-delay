b5b01471272a70800f04c26209f8bcaa1c3553e4
Maximilian Konzack <m.p.konzack@tue.nl>
2015-10-09 15:13:48 +0200

    Int Triple class to use it in the TripletMatching and the DP

---
+++ src/utils/IntTriple.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author max
+ */
+public class IntTriple {
+    public final int i;
+    public final int j;
+    private final int k;
+        
+    public IntTriple(int i, int j, int k) {
+        this.i = i;
+        this.j = j;
+        this.k = k;
+    }
+
+    public static IntTriple createIntTriple(int i, int j, int k) {
+        return new IntTriple(i, j, k);
+    }
+}
