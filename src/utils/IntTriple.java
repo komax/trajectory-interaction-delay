b1ca0e6ddf1903d134ed6fb76dcbff05685da497
Maximilian Konzack <m.p.konzack@tue.nl>
2015-10-12 13:26:12 +0200

    Generated equals and hashCode for IntTriple

--- src/utils/IntTriple.java
+++ src/utils/IntTriple.java
@@ -24,6 +24,38 @@ public class IntTriple {
         return this.i == i && this.j == i && this.k == k;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.i;
+        hash = 29 * hash + this.j;
+        hash = 29 * hash + this.k;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final IntTriple other = (IntTriple) obj;
+        if (this.i != other.i) {
+            return false;
+        }
+        if (this.j != other.j) {
+            return false;
+        }
+        if (this.k != other.k) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
     public static IntTriple createIntTriple(int i, int j, int k) {
         return new IntTriple(i, j, k);
     }
