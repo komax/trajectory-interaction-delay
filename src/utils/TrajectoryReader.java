a5b67b57ffd914802500d18d5eb3606ded729215
Maximilian Konzack <m.p.konzack@tue.nl>
2016-11-14 15:22:43 +0100

    Handling csv files with longer duration of one trajectory than the other one

--- src/utils/TrajectoryReader.java
+++ src/utils/TrajectoryReader.java
@@ -15,73 +15,91 @@ import java.util.List;
  * @author max
  */
 public class TrajectoryReader {
+
     private Trajectory trajectory1;
     private Trajectory trajectory2;
     private final BufferedReader inputReader;
-    
+
     public static TrajectoryReader createTrajectoryReader(String fileName, boolean skipHeader) throws Exception {
         return new TrajectoryReader(fileName, skipHeader);
     }
-    
+
     public TrajectoryReader(String fileName, boolean skipHeader) throws Exception {
         this.inputReader = new BufferedReader(new FileReader(fileName));
-        
+
         // Read content of the file into the fields.
         readTrajectories(skipHeader);
     }
-    
+
     private void readTrajectories(boolean skipHeader) throws IOException {
         this.trajectory1 = new Trajectory();
         this.trajectory2 = new Trajectory();
-        
+
         if (skipHeader) {
             // Skip the first line.
             inputReader.readLine();
         }
-        
-        String currentLine = "";
-        
+
+        String currentLine;
+
         // Read the lines and their data points.
         while ((currentLine = inputReader.readLine()) != null) {
             String[] lineElements = currentLine.split("\\s+");
             double[] pointsTraject1 = new double[2];
             double[] pointsTraject2 = new double[2];
-            if (lineElements[0].equals("")) {
+
+            if (lineElements.length == 6) {
+                if (lineElements[0].equals("")) {
 //                System.err.println(lineElements[1] + "|" + lineElements[2] + "|" + lineElements[3] + "|" + lineElements[4] + "|" + lineElements[5]);
-                pointsTraject1[0] = Double.parseDouble(lineElements[2]);
-                pointsTraject1[1] = Double.parseDouble(lineElements[3]);
-                pointsTraject2[0] = Double.parseDouble(lineElements[4]);
-                pointsTraject2[1] = Double.parseDouble(lineElements[5]);
-            } else {
+                    pointsTraject1[0] = Double.parseDouble(lineElements[2]);
+                    pointsTraject1[1] = Double.parseDouble(lineElements[3]);
+                    pointsTraject2[0] = Double.parseDouble(lineElements[4]);
+                    pointsTraject2[1] = Double.parseDouble(lineElements[5]);
+                } else {
 //                System.err.println(lineElements[0] + "|" + lineElements[1] + "|" + lineElements[2] + "|" + lineElements[3] + "|" + lineElements[4]);
-                pointsTraject1[0] = Double.parseDouble(lineElements[1]);
-                pointsTraject1[1] = Double.parseDouble(lineElements[2]);
-                pointsTraject2[0] = Double.parseDouble(lineElements[3]);
-                pointsTraject2[1] = Double.parseDouble(lineElements[4]);
+                    pointsTraject1[0] = Double.parseDouble(lineElements[1]);
+                    pointsTraject1[1] = Double.parseDouble(lineElements[2]);
+                    pointsTraject2[0] = Double.parseDouble(lineElements[3]);
+                    pointsTraject2[1] = Double.parseDouble(lineElements[4]);
+                }
+                trajectory1.addPoint(pointsTraject1);
+                trajectory2.addPoint(pointsTraject2);
+            } else if (lineElements.length == 4) {
+                // Skip Adding points to trajectory2.
+                if (lineElements[0].equals("")) {
+//                System.err.println(lineElements[1] + "|" + lineElements[2] + "|" + lineElements[3] + "|" + lineElements[4] + "|" + lineElements[5]);
+                    pointsTraject1[0] = Double.parseDouble(lineElements[2]);
+                    pointsTraject1[1] = Double.parseDouble(lineElements[3]);
+                } else {
+//                System.err.println(lineElements[0] + "|" + lineElements[1] + "|" + lineElements[2] + "|" + lineElements[3] + "|" + lineElements[4]);
+                    pointsTraject1[0] = Double.parseDouble(lineElements[1]);
+                    pointsTraject1[1] = Double.parseDouble(lineElements[2]);
+                }
+                trajectory1.addPoint(pointsTraject1);
+            } else {
+                throw new IOException("Illegal format for a csv file");
             }
-            trajectory1.addPoint(pointsTraject1);
-            trajectory2.addPoint(pointsTraject2);
         }
     }
-    
+
     private double[][] convertTrajectoryRecursively(List<double[]> traject, int dimensions) {
         int length = traject.size();
         double[][] result = new double[length][dimensions];
-        
+
         for (int i = 0; i < length; i++) {
-           double[] pointTraject = traject.get(i);
-           result[i] = pointTraject;
+            double[] pointTraject = traject.get(i);
+            result[i] = pointTraject;
         }
-        
+
         return result;
     }
-    
+
     public Trajectory getTrajectory1() {
         return trajectory1;
     }
-    
+
     public Trajectory getTrajectory2() {
         return trajectory2;
     }
-    
+
 }
